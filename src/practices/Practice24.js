5670acf80f3fc78ae9718aefad9e012d456be987
Shunze Lin <4A490101@stust.edu.tw>
2021-03-23 11:16:46 +0800

    Practice22: Basic Text Layout 8

--- src/practices/Practice24.js
+++ src/practices/Practice24.js
@@ -89,6 +89,70 @@ function Practice24() {
 						</p>
 					</div>
 				</div>
+
+				<div
+					css={`
+						padding: 5rem 0;
+						max-width: 120rem;
+						max-height: 40rem;
+						margin: auto;
+						display: flex;
+					`}
+				>
+					<h1
+						css={`
+							flex: 0 0 39rem;
+							margin-left: 2rem;
+							color: #aaa;
+							font-size: 5rem;
+							transform: rotate(90deg);
+							transform-origin: top left;
+							position: relative;
+							left: 39rem;
+						`}
+					>
+						CSS Coke is awesome
+					</h1>
+					<div
+						css={`
+							flex: 0 0 79rem;
+							column-count: 2;
+							column-gap: 2rem;
+							box-shadow: 0 0 2rem rgba(70, 70, 70, 0.3);
+							padding: 1.5rem;
+							& > p {
+								margin-bottom: 1rem;
+							}
+							& > p:first-child:first-letter {
+								font-size: 4rem;
+								float: left;
+								padding-right: 1rem;
+							}
+						`}
+					>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Nulla natus in totam
+							nostrum tempora voluptatum hic architecto amet optio nam laudantium, modi illum
+							eius rem quod animi temporibus, quibusdam, odit. Ipsa, voluptatem sequi
+							praesentium a id quas reiciendis unde consequatur ipsam laudantium, hic
+							voluptatibus, et cum iure ab assumenda quae.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Nulla natus in totam
+							nostrum tempora voluptatum hic architecto amet optio nam laudantium, modi illum
+							eius rem quod animi temporibus, quibusdam, odit. Ipsa, voluptatem sequi
+							praesentium a id quas reiciendis unde consequatur ipsam laudantium, hic
+							voluptatibus, et cum iure ab assumenda quae.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Nulla natus in totam
+							nostrum tempora voluptatum hic architecto amet optio nam laudantium, modi illum
+							eius rem quod animi temporibus, quibusdam, odit. Ipsa, voluptatem sequi
+							praesentium a id quas reiciendis unde consequatur ipsam laudantium, hic
+							voluptatibus, et cum iure ab assumenda quae.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
