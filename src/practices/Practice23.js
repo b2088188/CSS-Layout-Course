a9bf9a9380be900c5afa80b03272d01939b91510
Shunze Lin <4A490101@stust.edu.tw>
2021-03-23 09:38:49 +0800

    Practice22: Basic Text Layout 6

--- src/practices/Practice23.js
+++ src/practices/Practice23.js
@@ -40,11 +40,11 @@ function Practice23() {
 					</h1>
 					<div
 						css={`
+							column-count: 2;
+							column-gap: 46rem;
 							& > p {
 								line-height: 1.6;
 								margin-bottom: 2rem;
-								column-count: 2;
-								column-gap: 46rem;
 							}
 						`}
 					>
@@ -76,6 +76,7 @@ function Practice23() {
 						max-width: 120rem;
 						margin: auto;
 						display: flex;
+						padding: 5rem 0;
 					`}
 				>
 					<h1
@@ -95,6 +96,76 @@ function Practice23() {
 								bottom: 0;
 								left: 2.5rem;
 								z-index: -1;
+							}
+						`}
+					>
+						CSS Coke is awesome
+					</h1>
+					<div
+						css={`
+							flex: 0 0 80rem;
+							order: -1;
+							column-count: 2;
+							column-gap: 2rem;
+							& > p {
+								margin-bottom: 1rem;
+							}
+							& > p:first-child:first-letter {
+								font-size: 4rem;
+								padding-right: 1rem;
+								float: left;
+							}
+						`}
+					>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugit facere
+							ratione, porro quo asperiores explicabo necessitatibus recusandae suscipit.
+							Maxime tenetur optio quaerat inventore aliquid molestiae, dicta tempore eaque,
+							debitis natus adipisci vel eligendi vero, iusto nobis voluptas. Nisi pariatur
+							molestias itaque doloribus, voluptatum animi, a totam numquam nam qui.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugit facere
+							ratione, porro quo asperiores explicabo necessitatibus recusandae suscipit.
+							Maxime tenetur optio quaerat inventore aliquid molestiae, dicta tempore eaque,
+							debitis natus adipisci vel eligendi vero, iusto nobis voluptas. Nisi pariatur
+							molestias itaque doloribus, voluptatum animi, a totam numquam nam qui.
+						</p>
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugit facere
+							ratione, porro quo asperiores explicabo necessitatibus recusandae suscipit.
+							Maxime tenetur optio quaerat inventore aliquid molestiae, dicta tempore eaque,
+							debitis natus adipisci vel eligendi vero, iusto nobis voluptas. Nisi pariatur
+							molestias itaque doloribus, voluptatum animi, a totam numquam nam qui.
+						</p>
+					</div>
+				</div>
+				<div
+					css={`
+						padding: 5rem 0;
+						max-width: 120rem;
+						margin: auto;
+						display: flex;
+					`}
+				>
+					<h1
+						css={`
+							flex: 0 0 40rem;
+							padding: 1.5rem;
+							font-size: 5rem;
+							color: #aaa;
+							position: relative;
+							&:after {
+								content: '';
+								display: block;
+								border: solid 2.5rem #f1c555;
+								height: 15rem;
+								width: 15rem;
+								position: absolute;
+								bottom: 0;
+								left: 2.5rem;
+								z-index: -1;
+								transform: rotate(20deg);
 							}
 						`}
 					>
